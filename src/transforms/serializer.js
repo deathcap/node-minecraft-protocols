e14c1cdfa639c06f926461629089801ee2a1d9c0
deathcap <deathcap@gmx.co.uk>
2016-02-01 23:06:35 -0800

    Update transforms/serializer to use generated protocol JSON
    Ref https://github.com/PrismarineJS/node-minecraft-protocol/issues/301

--- src/transforms/serializer.js
+++ src/transforms/serializer.js
@@ -6,39 +6,24 @@ var Parser = require("protodef").Parser;
 
 var minecraft = require("../datatypes/minecraft");
 var states = require("../states");
+var protocolSpecs = require('../protocol');
 
 function createProtocol(version, state, direction)
 {
-  var mcData=require("minecraft-data")(version);
-  var packets = mcData.protocol.states[state][direction];
-  var types = mcData.protocol.types;
-
   var proto = new ProtoDef();
   proto.addType("string",["pstring",{
     countType:"varint"
   }]);
   proto.addTypes(minecraft);
-  proto.addTypes(types);
 
-  Object.keys(packets).forEach(function(name) {
-    proto.addType("packet_"+name,["container",packets[name].fields]);
+  if (!protocolSpecs[version]) throw new Error(`no protocol specification for version: ${version}`);
+
+  var protocolSpec = protocolSpecs[version][state][direction];
+
+  Object.keys(protocolSpec).forEach((name) => {
+    proto.addType(name, protocolSpec[name]);
   });
 
-  proto.addType("packet",["container", [
-    { "name": "name", "type":["mapper",{"type": "varint" ,
-      "mappings":Object.keys(packets).reduce(function(acc,name){
-        acc[parseInt(packets[name].id)]=name;
-        return acc;
-      },{})
-    }]},
-    { "name": "params", "type": ["switch", {
-      "compareTo": "name",
-      "fields": Object.keys(packets).reduce(function(acc,name){
-        acc[name]="packet_"+name;
-        return acc;
-      },{})
-    }]}
-  ]]);
   return proto;
 }
 
